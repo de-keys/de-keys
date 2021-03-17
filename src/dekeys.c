1b6d02e3b921fd0ebc1ba1f42128ca6737ee5aef
MhmCats <71032999+MhmCats@users.noreply.github.com>
2021-03-17 12:27:07 +0000

    Initial commit

---
+++ src/dekeys.c
@@ -0,0 +1,110 @@
+#include <windows.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+const char g_szClassName[] = "myWindowClass";
+
+bool mousedown = false;
+POINT lastLocation;
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+        break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+
+        case WM_LBUTTONDOWN: {
+            printf("a");
+            mousedown = true;
+            GetCursorPos(&lastLocation);
+            RECT rect;
+            GetWindowRect(hwnd, &rect);
+            lastLocation.x = lastLocation.x - rect.left;
+            lastLocation.y = lastLocation.y - rect.top;
+            break;
+        }
+        case WM_LBUTTONUP: {
+            printf("b");
+            mousedown = false;
+            break;
+        }
+        case WM_MOUSEMOVE: {
+            printf("c");
+            if (mousedown) {
+                printf("THE MOUSE IS DOWN!!!!!!!!!!!!!");
+                POINT currentpos;
+                GetCursorPos(&currentpos);
+                int x =  currentpos.x - lastLocation.x;
+                int y =  currentpos.y - lastLocation.y;
+                MoveWindow(hwnd, x, y, 50, 50, false);
+                lastLocation = currentpos;
+            }
+            break;
+        }
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG Msg;
+
+    //Step 1: Registering the Window Class
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, "Window Registration Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        g_szClassName,
+        "French Keys",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 500, 0,
+        NULL, NULL, hInstance, NULL);
+
+    SetWindowLong(hwnd, GWL_STYLE, WS_BORDER );
+
+    if(hwnd == NULL)
+    {
+        MessageBox(NULL, "Window Creation Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+
+    while(GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+    return Msg.wParam;
+}
