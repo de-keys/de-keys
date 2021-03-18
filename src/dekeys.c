dfb8969aa37576b68703e1258540e35f97b5d359
HyperCube <0hypercube@gmail.com>
2021-03-18 19:57:41 +0000

    Add callback for close button

--- src/dekeys.c
+++ src/dekeys.c
@@ -10,25 +10,30 @@ bool mousedown = false;
 
 POINT lastPos;
 
-int winSizeX = 500;
-int winSizeY = 0;
+const int winSizeX = 900;
+const int winSizeY = 0;
 
 // lessen sie die docs: https://docs.microsoft.com/en-us/windows/win32/winmsg/lowlevelmouseproc
 LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
+	// It said to do this in the docs, and we don't want to annoy micro-soft
 	if(nCode<0){
 		return CallNextHookEx(NULL, nCode, wParam, lParam);
 	}
 
 	switch (wParam)
 	{
+	// Handle pressing down
 	case WM_LBUTTONDOWN:
 		mousedown = true;
 		GetCursorPos(&lastPos);
 		break;
+	// Handle releasing mouse
 	case WM_LBUTTONUP:
 		mousedown = false;
 		break;
+	// Handle dragging mouse
+	// TODO: stop dragging if mouse far away from window
 	case WM_MOUSEMOVE:
 		if (mousedown) {
 			POINT currentpos;
@@ -53,12 +58,20 @@ LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 void MakeThoseButtons(HINSTANCE hInst, HWND hWnd){
 	// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexw
 
+	static HWND title;
+	title = CreateWindow( "static", "de-keys",
+				WS_CHILD | WS_VISIBLE | BS_TEXT | SS_CENTER | DS_SETFONT,
+				0, 0, 
+				80, 40,
+				hWnd, (HMENU) 0,
+				hInst, NULL );
+
 	static HWND hButton;
 	hButton = CreateWindow( "button", "Label",
 				WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
-				0, 0, 
-				50, 40,
-				hWnd, (HMENU) 6,
+				80, 0, 
+				80, 40,
+				hWnd, (HMENU) 5,
 				hInst, NULL );
 
 	static HWND closeButton;
@@ -66,7 +79,7 @@ void MakeThoseButtons(HINSTANCE hInst, HWND hWnd){
 				WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
 				winSizeX-40, 0, 
 				40, 40,
-				hWnd, (HMENU) 6,
+				hWnd, (HMENU) 99,
 				hInst, NULL );
 
 	printf("Made those buttons!!");
@@ -77,13 +90,27 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	hwndMain = hwnd;
 	switch(msg)
 	{
+		// Handle buttons
+		case WM_COMMAND:
+			switch (wParam)
+			{
+			// Handle exit button
+			case 99:
+				PostMessage(hwndMain,WM_CLOSE,0,0);
+				break;
+			case 5:
+				MessageBox(NULL, TEXT("Label button pressed!"), TEXT("Title"), 0);
+			
+			default:
+				break;
+			}
+			break;
 		case WM_CLOSE:
 			DestroyWindow(hwnd);
 			break;
 		case WM_DESTROY:
 			PostQuitMessage(0);
 			break;
-
 		default:
 			return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
@@ -99,7 +126,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	hinstMain = hInstance;
 
-	//Step 1: Registering the Window Class
+	// Registering the Window Class
 	wc.cbSize			= sizeof(WNDCLASSEX);
 	wc.style			= 0;
 	wc.lpfnWndProc		= WndProc;
@@ -120,6 +147,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		return 0;
 	}
 
+	// Create the window
 	hwnd = CreateWindowEx(
 		WS_EX_TRANSPARENT,
 		g_szClassName,
@@ -136,22 +164,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 			MB_ICONEXCLAMATION | MB_OK);
 		return 0;
 	}
+
+	// Initalise the window
 	hwndMain = hwnd;
 	MakeThoseButtons(hInstance, hwndMain);
-
 	SetWindowPos(hwnd,HWND_TOPMOST, 0, 0, winSizeX, winSizeY, SWP_SHOWWINDOW);
 
-	
-
+	// Show the window
 	ShowWindow(hwnd, SW_SHOW);
 	UpdateWindow(hwnd);
 
 	
-
+	// Add a mouse hook
 	HOOKPROC mouseLL = &LowLevelMouseProc;
 	HHOOK mouseLLHook;
 	mouseLLHook = SetWindowsHookEx(WH_MOUSE_LL, mouseLL, hInstance, 0);
 
+	// Handle the event loop
 	while(GetMessage(&Msg, NULL, 0, 0) > 0)
 	{
 		TranslateMessage(&Msg);
