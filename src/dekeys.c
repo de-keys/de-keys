9c49985fb6907667af695de567e8fe8ae48c2266
HyperCube <0hypercube@gmail.com>
2021-03-18 18:07:18 +0000

    Hooks for dragging. No lag.

--- src/dekeys.c
+++ src/dekeys.c
@@ -2,60 +2,65 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+HWND hwndMain;
 const char g_szClassName[] = "myWindowClass";
 
 bool mousedown = false;
 
 POINT lastPos;
 
-LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam)
+// lessen sie die docs: https://docs.microsoft.com/en-us/windows/win32/winmsg/lowlevelmouseproc
+LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-   printf("tap");
+	if(nCode<0){
+		return CallNextHookEx(NULL, nCode, wParam, lParam);
+	}
 
-   return CallNextHookEx(NULL, nCode, wParam, lParam);
+	switch (wParam)
+	{
+	case WM_LBUTTONDOWN:
+		mousedown = true;
+		GetCursorPos(&lastPos);
+		break;
+	case WM_LBUTTONUP:
+		mousedown = false;
+		break;
+	case WM_MOUSEMOVE:
+		if (mousedown) {
+			POINT currentpos;
+			GetCursorPos(&currentpos);
+			int x = currentpos.x - lastPos.x;
+			int y = currentpos.y - lastPos.y;
+			RECT rc;
+			GetWindowRect(hwndMain,&rc);
+
+			MoveWindow(hwndMain, rc.left + x,rc.top + y, 500, 0, false);
+
+			lastPos = currentpos;
+		}
+		break;
+	default:
+		break;
+	}
+	printf("tap");
+
+	return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	hwndMain = hwnd;
 	switch(msg)
 	{
 		case WM_CLOSE:
 			DestroyWindow(hwnd);
-		break;
+			break;
 		case WM_DESTROY:
 			PostQuitMessage(0);
-		break;
+			break;
+
 		default:
 			return DefWindowProc(hwnd, msg, wParam, lParam);
-
-		case WM_LBUTTONDOWN: {
-			mousedown = true;
-			printf("mouse down");
-
-			GetCursorPos(&lastPos);
-
-			break;
-		}
-		case WM_LBUTTONUP: {
-			mousedown = false;
-			printf("mouse up");
-			break;
-		}
-		case WM_MOUSEMOVE: {
-			if (mousedown) {
-				POINT currentpos;
-				GetCursorPos(&currentpos);
-				int x = currentpos.x - lastPos.x;
-				int y = currentpos.y - lastPos.y;
-				RECT rc;
-				GetWindowRect(hwnd,&rc);
-
-				MoveWindow(hwnd, rc.left + x,rc.top + y, 500, 0, false);
-
-				lastPos = currentpos;
-			}
-			break;
-		}
 	}
 	return 0;
 }
@@ -63,24 +68,23 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
 {
-    HOOKPROC hookproc;
 	WNDCLASSEX wc;
 	HWND hwnd;
 	MSG Msg;
 
 	//Step 1: Registering the Window Class
-	wc.cbSize		= sizeof(WNDCLASSEX);
-	wc.style		 = 0;
-	wc.lpfnWndProc   = WndProc;
-	wc.cbClsExtra	= 0;
-	wc.cbWndExtra	= 0;
-	wc.hInstance	 = hInstance;
-	wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hCursor	   = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	wc.lpszMenuName  = NULL;
-	wc.lpszClassName = g_szClassName;
-	wc.hIconSm	   = LoadIcon(NULL, IDI_APPLICATION);
+	wc.cbSize			= sizeof(WNDCLASSEX);
+	wc.style			= 0;
+	wc.lpfnWndProc		= WndProc;
+	wc.cbClsExtra		= 0;
+	wc.cbWndExtra		= 0;
+	wc.hInstance		= hInstance;
+	wc.hIcon			= LoadIcon(NULL, IDI_APPLICATION);	// TODO: change this to be the Eszett
+	wc.hCursor			= LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wc.lpszMenuName 	= NULL;
+	wc.lpszClassName	= g_szClassName;
+	wc.hIconSm			= LoadIcon(NULL, IDI_APPLICATION);	// TODO: change this to be the Eszett
 
 	if(!RegisterClassEx(&wc))
 	{
@@ -106,11 +110,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		return 0;
 	}
 
+	SetWindowPos(hwnd,HWND_TOPMOST, 0, 0, 500, 0, SWP_SHOWWINDOW);
+	
 	ShowWindow(hwnd, SW_SHOW);
 	UpdateWindow(hwnd);
 
-	SetWindowPos(hwnd,HWND_TOPMOST, 0, 0, 500, 0, SWP_SHOWWINDOW);
-	// SetWindowsHookEx(WH_CALLWNDPROC, hookproc, hInstance, 0);
+	
+
+	HOOKPROC mouseLL = &LowLevelMouseProc;
+	HHOOK mouseLLHook;
+	mouseLLHook = SetWindowsHookEx(WH_MOUSE_LL, mouseLL, hInstance, 0);
 
 	while(GetMessage(&Msg, NULL, 0, 0) > 0)
 	{
