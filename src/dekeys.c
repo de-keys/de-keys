285b786ecd58a9b752787f00ee4989dcdb9941a1
MhmCats <71032999+MhmCats@users.noreply.github.com>
2021-03-18 13:37:48 +0000

    Dragging is working!

--- src/dekeys.c
+++ src/dekeys.c
@@ -8,6 +8,13 @@ bool mousedown = false;
 
 POINT lastPos;
 
+LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+   printf("tap");
+
+   return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch(msg)
@@ -40,11 +47,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 				GetCursorPos(&currentpos);
 				int x = currentpos.x - lastPos.x;
 				int y = currentpos.y - lastPos.y;
-
 				RECT rc;
-				GetClientRect(hwnd,&rc);
+				GetWindowRect(hwnd,&rc);
 
-				MoveWindow(hwnd, rc.top, rc.left, 500, 0, false);
+				MoveWindow(hwnd, rc.left + x,rc.top + y, 500, 0, false);
 
 				lastPos = currentpos;
 			}
@@ -57,6 +63,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
 {
+    HOOKPROC hookproc;
 	WNDCLASSEX wc;
 	HWND hwnd;
 	MSG Msg;
@@ -85,7 +92,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	hwnd = CreateWindowEx(
 		WS_EX_CLIENTEDGE,
 		g_szClassName,
-		"French Keys",
+		"de-keys",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT, 500, 0,
 		NULL, NULL, hInstance, NULL);
@@ -101,6 +108,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	ShowWindow(hwnd, SW_SHOW);
 	UpdateWindow(hwnd);
+
+	SetWindowPos(hwnd,HWND_TOPMOST, 0, 0, 500, 0, SWP_SHOWWINDOW);
+	// SetWindowsHookEx(WH_CALLWNDPROC, hookproc, hInstance, 0);
 
 	while(GetMessage(&Msg, NULL, 0, 0) > 0)
 	{
